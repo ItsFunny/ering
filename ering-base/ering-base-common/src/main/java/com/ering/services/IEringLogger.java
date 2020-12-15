632586675e544bc742fe02a288fb06d34a9096aa
lvcong <lvcong@ffpms.com>
2020-12-15 21:54:09 +0800

    save

---
+++ ering-base/ering-base-common/src/main/java/com/ering/services/IEringLogger.java
@@ -0,0 +1,14 @@
+package com.ering.services;
+
+/**
+ * @author Charlie
+ * @When
+ * @Description
+ * @Detail
+ * @Attention:
+ * @Date 创建时间：2020-12-15 20:07
+ */
+public interface IEringLogger
+{
+    void debug();
+}
